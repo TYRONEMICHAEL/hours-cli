dfdeb681fbb7fa03dbbf98a6d7c07eca1d011cc4
TYRONEMICHAEL <tyronemichael@gmail.com>
2015-11-29 08:07:35 +0200

    Chain prompts

--- index.js
+++ index.js
@@ -4,6 +4,25 @@ const banner = 'Welcome to the standalone Vorpal server.';
 const delimiter = 'hours-cli$';
 const cli = new Vorpal();
 
+const start = (context) => {
+  return new Promise((resolve, reject) => {
+    context.prompt({
+      type: 'input',
+      name: 'start',
+      default: Number,
+      message: `Please enter the date (YYYY-MM-DD) of when you would like to START submitting your hours: `,
+    }, (result) => {
+      resolve();
+    });
+  });
+};
+
+cli
+  .command('start', 'Start submitting your hours')
+  .action(function(args, cb) {
+    return start(this).then(() => start(this)).then(cb);
+  });
+
 cli
   .delimiter('hours-cli$:')
   .show()
