0fec2ce9a8134f62b848df79a824842d400b1a04
TYRONEMICHAEL <tyronemichael@gmail.com>
2015-12-13 11:25:00 +0200

    Include lib folder

--- index.js
+++ index.js
@@ -1,39 +1,43 @@
 import Vorpal from 'vorpal';
-import moment from 'moment';
+import credentials from './credentials';
 import hours from './hours';
+import getDates from './lib/getDates';
+import hoursService from './lib/hoursService';
 
-const fromDate = moment(hours.from);
-const toDate = moment(hours.to);
-
-
-
-const banner = 'Welcome to the standalone Vorpal server.';
-const delimiter = 'hours-cli$';
-const cli = new Vorpal();
+hoursService
+  .getAccessToken(credentials.tangentUsername, credentials.tangentPassword)
+  .then((body) => console.log(body))
+  .catch((err) => console.log(err));
 
 
 
-const start = (context) => {
-  return new Promise((resolve, reject) => {
-    context.prompt({
-      type: 'input',
-      name: 'start',
-      default: Number,
-      message: `Please enter the date (YYYY-MM-DD) of when you would like to START submitting your hours: `,
-    }, (result) => {
-      resolve();
-    });
-  });
-};
-
-cli
-  .command('start', 'Start submitting your hours')
-  .action(function(args, cb) {
-    return start(this).then(() => start(this)).then(cb);
-  });
-
-cli
-  .delimiter('hours-cli$:')
-  .show()
-  .log('Welcome to the Tangent Solution CLI hours service.')
-  .exec('help');
+// const banner = 'Welcome to the standalone Vorpal server.';
+// const delimiter = 'hours-cli$';
+// const cli = new Vorpal();
+//
+//
+//
+// const start = (context) => {
+//   return new Promise((resolve, reject) => {
+//     context.prompt({
+//       type: 'input',
+//       name: 'start',
+//       default: Number,
+//       message: `Please enter the date (YYYY-MM-DD) of when you would like to START submitting your hours: `,
+//     }, (result) => {
+//       resolve();
+//     });
+//   });
+// };
+//
+// cli
+//   .command('start', 'Start submitting your hours')
+//   .action(function(args, cb) {
+//     return start(this).then(() => start(this)).then(cb);
+//   });
+//
+// cli
+//   .delimiter('hours-cli$:')
+//   .show()
+//   .log('Welcome to the Tangent Solution CLI hours service.')
+//   .exec('help');
